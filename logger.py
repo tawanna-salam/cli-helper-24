a1c0557c0fbb4193cccea2b130015906c57b2fe1
Tawanna Salam <tawanna-salam@inbox.lv>
2026-08-09 12:09:11 +0300

    fix input validation in main loop

--- logger.py
+++ logger.py
@@ -1,29 +1,28 @@
 import logging
 
+# Configure logging settings
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Logger:
     def __init__(self, name):
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-
-    def debug(self, message):
-        self.logger.debug(message)
 
     def info(self, message):
         self.logger.info(message)
 
-    def warning(self, message):
-        self.logger.warning(message)
-
     def error(self, message):
         self.logger.error(message)
 
-    def critical(self, message):
-        self.logger.critical(message)
+    def warn(self, message):
+        self.logger.warning(message)
 
-# Example usage:
-# logger = Logger('my_logger')
-# logger.info('This is an info message')
+    def debug(self, message):
+        self.logger.debug(message)
+
+# Example usage
+if __name__ == '__main__':
+    log = Logger('cli-helper-24')
+    log.info('This is an info message')
+    log.debug('This is a debug message')
+    log.warn('This is a warning message')
+    log.error('This is an error message')
